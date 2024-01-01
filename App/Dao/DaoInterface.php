b4f4ac9e4b328bfb1b3a03cd522016765e268059
GhizlaneMeq <meqdarghizlane@gmail.com>
2024-01-01 13:17:28 +0100

    Creation du interface

---
+++ App/Dao/DaoInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+interface DaoInterface
+{
+    public function getAll();
+    public function save($entity);
+    public function update($entity);
+    public function getById($id);
+    public function deleteById($id);
+
+}
